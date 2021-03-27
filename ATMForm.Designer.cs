fd01a3a11f1ee2a79d50d2a158e13a42481acca9
Khaelem Watt <55397210+khaelemwatt@users.noreply.github.com>
2021-03-27 23:17:01 +0000

    Updated UI and added semaphore
    Added a good looking user interface and a semaphore to stop a race condition from happening

---
+++ ATMForm.Designer.cs
@@ -0,0 +1,363 @@
+﻿namespace ATM
+{
+    partial class ATMForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.input = new System.Windows.Forms.TextBox();
+            this.enter = new System.Windows.Forms.Button();
+            this.output = new System.Windows.Forms.RichTextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.button9 = new System.Windows.Forms.Button();
+            this.button0 = new System.Windows.Forms.Button();
+            this.side1 = new System.Windows.Forms.Button();
+            this.side2 = new System.Windows.Forms.Button();
+            this.side3 = new System.Windows.Forms.Button();
+            this.clearButton = new System.Windows.Forms.Button();
+            this.cancelButton = new System.Windows.Forms.Button();
+            this.side4 = new System.Windows.Forms.Button();
+            this.blankButton1 = new System.Windows.Forms.Button();
+            this.blankButton2 = new System.Windows.Forms.Button();
+            this.blankButton3 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // input
+            // 
+            this.input.Location = new System.Drawing.Point(59, 149);
+            this.input.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.input.Name = "input";
+            this.input.Size = new System.Drawing.Size(154, 22);
+            this.input.TabIndex = 1;
+            this.input.Visible = false;
+            // 
+            // enter
+            // 
+            this.enter.BackColor = System.Drawing.Color.LightGreen;
+            this.enter.Font = new System.Drawing.Font("Tahoma", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.enter.Location = new System.Drawing.Point(347, 289);
+            this.enter.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.enter.Name = "enter";
+            this.enter.Size = new System.Drawing.Size(76, 49);
+            this.enter.TabIndex = 2;
+            this.enter.Text = "Enter";
+            this.enter.UseVisualStyleBackColor = false;
+            this.enter.Click += new System.EventHandler(this.enter_Click);
+            // 
+            // output
+            // 
+            this.output.BackColor = System.Drawing.Color.Honeydew;
+            this.output.DetectUrls = false;
+            this.output.Font = new System.Drawing.Font("Gill Sans MT", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.output.Location = new System.Drawing.Point(59, 5);
+            this.output.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.output.Name = "output";
+            this.output.ReadOnly = true;
+            this.output.ScrollBars = System.Windows.Forms.RichTextBoxScrollBars.None;
+            this.output.Size = new System.Drawing.Size(364, 140);
+            this.output.TabIndex = 0;
+            this.output.Text = "";
+            // 
+            // button1
+            // 
+            this.button1.FlatAppearance.BorderColor = System.Drawing.Color.Black;
+            this.button1.Font = new System.Drawing.Font("Calibri", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(59, 176);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(70, 50);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "1";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.FlatAppearance.BorderColor = System.Drawing.Color.Black;
+            this.button2.Font = new System.Drawing.Font("Calibri", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(135, 176);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(70, 50);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "2";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.FlatAppearance.BorderColor = System.Drawing.Color.Black;
+            this.button3.Font = new System.Drawing.Font("Calibri", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button3.Location = new System.Drawing.Point(211, 176);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(70, 50);
+            this.button3.TabIndex = 5;
+            this.button3.Text = "3";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            this.button4.FlatAppearance.BorderColor = System.Drawing.Color.Black;
+            this.button4.Font = new System.Drawing.Font("Calibri", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button4.Location = new System.Drawing.Point(59, 232);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(70, 50);
+            this.button4.TabIndex = 6;
+            this.button4.Text = "4";
+            this.button4.UseVisualStyleBackColor = true;
+            // 
+            // button5
+            // 
+            this.button5.FlatAppearance.BorderColor = System.Drawing.Color.Black;
+            this.button5.Font = new System.Drawing.Font("Calibri", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button5.Location = new System.Drawing.Point(135, 232);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(70, 50);
+            this.button5.TabIndex = 7;
+            this.button5.Text = "5";
+            this.button5.UseVisualStyleBackColor = true;
+            // 
+            // button6
+            // 
+            this.button6.FlatAppearance.BorderColor = System.Drawing.Color.Black;
+            this.button6.Font = new System.Drawing.Font("Calibri", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button6.Location = new System.Drawing.Point(211, 232);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(70, 50);
+            this.button6.TabIndex = 8;
+            this.button6.Text = "6";
+            this.button6.UseVisualStyleBackColor = true;
+            // 
+            // button7
+            // 
+            this.button7.FlatAppearance.BorderColor = System.Drawing.Color.Black;
+            this.button7.Font = new System.Drawing.Font("Calibri", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button7.Location = new System.Drawing.Point(59, 288);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(70, 50);
+            this.button7.TabIndex = 9;
+            this.button7.Text = "7";
+            this.button7.UseVisualStyleBackColor = true;
+            // 
+            // button8
+            // 
+            this.button8.FlatAppearance.BorderColor = System.Drawing.Color.Black;
+            this.button8.Font = new System.Drawing.Font("Calibri", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button8.Location = new System.Drawing.Point(135, 287);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(70, 50);
+            this.button8.TabIndex = 10;
+            this.button8.Text = "8";
+            this.button8.UseVisualStyleBackColor = true;
+            // 
+            // button9
+            // 
+            this.button9.FlatAppearance.BorderColor = System.Drawing.Color.Black;
+            this.button9.Font = new System.Drawing.Font("Calibri", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button9.Location = new System.Drawing.Point(211, 288);
+            this.button9.Name = "button9";
+            this.button9.Size = new System.Drawing.Size(70, 50);
+            this.button9.TabIndex = 11;
+            this.button9.Text = "9";
+            this.button9.UseVisualStyleBackColor = true;
+            // 
+            // button0
+            // 
+            this.button0.FlatAppearance.BorderColor = System.Drawing.Color.Black;
+            this.button0.Font = new System.Drawing.Font("Calibri", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button0.Location = new System.Drawing.Point(135, 343);
+            this.button0.Name = "button0";
+            this.button0.Size = new System.Drawing.Size(70, 50);
+            this.button0.TabIndex = 12;
+            this.button0.Text = "0";
+            this.button0.UseVisualStyleBackColor = true;
+            // 
+            // side1
+            // 
+            this.side1.Font = new System.Drawing.Font("Calibri", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.side1.Location = new System.Drawing.Point(12, 12);
+            this.side1.Name = "side1";
+            this.side1.Size = new System.Drawing.Size(41, 30);
+            this.side1.TabIndex = 13;
+            this.side1.Text = "1";
+            this.side1.UseVisualStyleBackColor = true;
+            this.side1.Click += new System.EventHandler(this.side1_Click);
+            // 
+            // side2
+            // 
+            this.side2.Font = new System.Drawing.Font("Calibri", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.side2.Location = new System.Drawing.Point(12, 45);
+            this.side2.Name = "side2";
+            this.side2.Size = new System.Drawing.Size(41, 30);
+            this.side2.TabIndex = 14;
+            this.side2.Text = "2";
+            this.side2.UseVisualStyleBackColor = true;
+            this.side2.Click += new System.EventHandler(this.side2_Click);
+            // 
+            // side3
+            // 
+            this.side3.Font = new System.Drawing.Font("Calibri", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.side3.Location = new System.Drawing.Point(12, 78);
+            this.side3.Name = "side3";
+            this.side3.Size = new System.Drawing.Size(41, 30);
+            this.side3.TabIndex = 15;
+            this.side3.Text = "3";
+            this.side3.UseVisualStyleBackColor = true;
+            this.side3.Click += new System.EventHandler(this.side3_Click);
+            // 
+            // clearButton
+            // 
+            this.clearButton.BackColor = System.Drawing.Color.Yellow;
+            this.clearButton.Font = new System.Drawing.Font("Tahoma", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.clearButton.Location = new System.Drawing.Point(347, 232);
+            this.clearButton.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.clearButton.Name = "clearButton";
+            this.clearButton.Size = new System.Drawing.Size(76, 50);
+            this.clearButton.TabIndex = 16;
+            this.clearButton.Text = "Clear";
+            this.clearButton.UseVisualStyleBackColor = false;
+            this.clearButton.Click += new System.EventHandler(this.clearButton_Click);
+            // 
+            // cancelButton
+            // 
+            this.cancelButton.BackColor = System.Drawing.Color.Red;
+            this.cancelButton.Font = new System.Drawing.Font("Tahoma", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cancelButton.Location = new System.Drawing.Point(347, 176);
+            this.cancelButton.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.cancelButton.Name = "cancelButton";
+            this.cancelButton.Size = new System.Drawing.Size(76, 50);
+            this.cancelButton.TabIndex = 18;
+            this.cancelButton.Text = "Cancel";
+            this.cancelButton.UseVisualStyleBackColor = false;
+            this.cancelButton.Click += new System.EventHandler(this.cancelButton_Click);
+            // 
+            // side4
+            // 
+            this.side4.Font = new System.Drawing.Font("Calibri", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.side4.Location = new System.Drawing.Point(11, 111);
+            this.side4.Name = "side4";
+            this.side4.Size = new System.Drawing.Size(41, 30);
+            this.side4.TabIndex = 22;
+            this.side4.Text = "4";
+            this.side4.UseVisualStyleBackColor = true;
+            this.side4.Click += new System.EventHandler(this.side4_Click);
+            // 
+            // blankButton1
+            // 
+            this.blankButton1.FlatAppearance.BorderColor = System.Drawing.Color.Black;
+            this.blankButton1.Font = new System.Drawing.Font("Calibri", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.blankButton1.Location = new System.Drawing.Point(59, 344);
+            this.blankButton1.Name = "blankButton1";
+            this.blankButton1.Size = new System.Drawing.Size(70, 50);
+            this.blankButton1.TabIndex = 23;
+            this.blankButton1.UseVisualStyleBackColor = true;
+            // 
+            // blankButton2
+            // 
+            this.blankButton2.FlatAppearance.BorderColor = System.Drawing.Color.Black;
+            this.blankButton2.Font = new System.Drawing.Font("Calibri", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.blankButton2.Location = new System.Drawing.Point(211, 344);
+            this.blankButton2.Name = "blankButton2";
+            this.blankButton2.Size = new System.Drawing.Size(70, 50);
+            this.blankButton2.TabIndex = 24;
+            this.blankButton2.UseVisualStyleBackColor = true;
+            // 
+            // blankButton3
+            // 
+            this.blankButton3.Font = new System.Drawing.Font("Calibri", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.blankButton3.Location = new System.Drawing.Point(347, 342);
+            this.blankButton3.Name = "blankButton3";
+            this.blankButton3.Size = new System.Drawing.Size(76, 49);
+            this.blankButton3.TabIndex = 25;
+            this.blankButton3.UseVisualStyleBackColor = true;
+            // 
+            // ATMForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.DarkGray;
+            this.ClientSize = new System.Drawing.Size(465, 409);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.blankButton3);
+            this.Controls.Add(this.blankButton2);
+            this.Controls.Add(this.blankButton1);
+            this.Controls.Add(this.side4);
+            this.Controls.Add(this.cancelButton);
+            this.Controls.Add(this.clearButton);
+            this.Controls.Add(this.side3);
+            this.Controls.Add(this.side2);
+            this.Controls.Add(this.side1);
+            this.Controls.Add(this.button0);
+            this.Controls.Add(this.button9);
+            this.Controls.Add(this.button8);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.enter);
+            this.Controls.Add(this.input);
+            this.Controls.Add(this.output);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.Name = "ATMForm";
+            this.Text = "ATM";
+            this.Load += new System.EventHandler(this.ATMForm_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.TextBox input;
+        private System.Windows.Forms.Button enter;
+        private System.Windows.Forms.RichTextBox output;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.Button button9;
+        private System.Windows.Forms.Button button0;
+        private System.Windows.Forms.Button side1;
+        private System.Windows.Forms.Button side2;
+        private System.Windows.Forms.Button side3;
+        private System.Windows.Forms.Button clearButton;
+        private System.Windows.Forms.Button cancelButton;
+        private System.Windows.Forms.Button side4;
+        private System.Windows.Forms.Button blankButton1;
+        private System.Windows.Forms.Button blankButton2;
+        private System.Windows.Forms.Button blankButton3;
+    }
+}
+

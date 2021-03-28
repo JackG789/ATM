556d197bcaecb6f8b3bc9d9a06415230445d127c
JackG789 <55397601+JackG789@users.noreply.github.com>
2021-03-28 16:43:21 +0100

    Add files via upload

---
+++ ATM-Khaelem/ATM/ATMMenu.Designer.cs
@@ -0,0 +1,201 @@
+﻿
+namespace ATM
+{
+    partial class ATMMenu
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.aboutToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.howToStartToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.startButton = new System.Windows.Forms.Button();
+            this.modeList = new System.Windows.Forms.ListBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.numList = new System.Windows.Forms.NumericUpDown();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.menuStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numList)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.aboutToolStripMenuItem,
+            this.howToStartToolStripMenuItem,
+            this.exitToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(702, 28);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // aboutToolStripMenuItem
+            // 
+            this.aboutToolStripMenuItem.Name = "aboutToolStripMenuItem";
+            this.aboutToolStripMenuItem.Size = new System.Drawing.Size(64, 26);
+            this.aboutToolStripMenuItem.Text = "About";
+            this.aboutToolStripMenuItem.Click += new System.EventHandler(this.aboutToolStripMenuItem_Click);
+            // 
+            // howToStartToolStripMenuItem
+            // 
+            this.howToStartToolStripMenuItem.Name = "howToStartToolStripMenuItem";
+            this.howToStartToolStripMenuItem.Size = new System.Drawing.Size(107, 26);
+            this.howToStartToolStripMenuItem.Text = "How to Start";
+            this.howToStartToolStripMenuItem.Click += new System.EventHandler(this.howToStartToolStripMenuItem_Click);
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(47, 26);
+            this.exitToolStripMenuItem.Text = "Exit";
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
+            // 
+            // startButton
+            // 
+            this.startButton.BackColor = System.Drawing.Color.MintCream;
+            this.startButton.Font = new System.Drawing.Font("Tahoma", 19.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.startButton.Location = new System.Drawing.Point(214, 402);
+            this.startButton.Name = "startButton";
+            this.startButton.Size = new System.Drawing.Size(248, 64);
+            this.startButton.TabIndex = 1;
+            this.startButton.Text = "Start ATM";
+            this.startButton.UseVisualStyleBackColor = false;
+            this.startButton.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // modeList
+            // 
+            this.modeList.BackColor = System.Drawing.Color.MintCream;
+            this.modeList.Font = new System.Drawing.Font("Tahoma", 19.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.modeList.FormattingEnabled = true;
+            this.modeList.ItemHeight = 40;
+            this.modeList.Items.AddRange(new object[] {
+            "Race Condition",
+            "Semaphore",
+            "Lock"});
+            this.modeList.Location = new System.Drawing.Point(214, 169);
+            this.modeList.Name = "modeList";
+            this.modeList.Size = new System.Drawing.Size(248, 124);
+            this.modeList.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Tahoma", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(12, 30);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(617, 48);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Assignment 3: ATM Simulator";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Tahoma", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(14, 78);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(645, 34);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "By Struan Robertson, Jack Glen and Khaelem Watt";
+            // 
+            // numList
+            // 
+            this.numList.BackColor = System.Drawing.Color.MintCream;
+            this.numList.Font = new System.Drawing.Font("Tahoma", 19.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.numList.Location = new System.Drawing.Point(214, 325);
+            this.numList.Maximum = new decimal(new int[] {
+            5,
+            0,
+            0,
+            0});
+            this.numList.Name = "numList";
+            this.numList.Size = new System.Drawing.Size(120, 47);
+            this.numList.TabIndex = 5;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Tahoma", 19.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(6, 202);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(188, 41);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "ATM Mode:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Tahoma", 19.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(6, 327);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(194, 41);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "ATM Count:";
+            // 
+            // ATMMenu
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.SeaShell;
+            this.ClientSize = new System.Drawing.Size(702, 494);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.numList);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.modeList);
+            this.Controls.Add(this.startButton);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "ATMMenu";
+            this.Text = "ATM Menu";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numList)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem aboutToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem howToStartToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.Button startButton;
+        private System.Windows.Forms.ListBox modeList;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.NumericUpDown numList;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+    }
+}
---
+++ ATM-Khaelem/ATM/Program.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+ 
+
+namespace ATM
+{
+    
+
+    public static class Global
+    {
+        //this is the semaphore which will control the access to the bank funds
+        //Will only allow one thread in at a time and starts with one space open
+        public static Semaphore _access = new Semaphore(1, 1);
+        public static object lockBal = new object();
+    }
+
+
+    public class Account
+    {
+        //the attributes for the account
+        private int balance;
+        private int pin;
+        private int accountNum;
+
+        // a constructor that takes initial values for each of the attributes (balance, pin, accountNumber)
+        public Account(int balance, int pin, int accountNum)
+        {
+            this.balance = balance;
+            this.pin = pin;
+            this.accountNum = accountNum;
+        }
+
+        //getter and setter functions for balance
+        public int getBalance()
+        {
+            return balance;
+        }
+        public void setBalance(int newBalance)
+        {
+            this.balance = newBalance;
+        }
+
+        /*
+         *   This funciton allows us to decrement the balance of an account
+         *   it perfomes a simple check to ensure the balance is greater than
+         *   the amount being debeted
+         *   
+         *   reurns:
+         *   true if the transactions if possible
+         *   false if there are insufficent funds in the account
+         */
+        public Boolean decrementBalance(int amount)
+        {
+            if (this.balance > amount)
+            {
+                //This is a manual implementation of a race condition
+                updateBalance(balance - amount);
+                //after this function, if race condition has been selected then the balance will be overwritten
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+
+            
+        }
+
+        public Boolean decrementBalLock(int amount)
+        {
+            lock (Global.lockBal)
+           {
+                if (this.balance > amount)
+                {
+                    updateBalance(balance - amount);
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+        }   
+        //This function is used to manually demonstrate the race condition by updating the balance from a function
+        public void updateBalance(int amount)
+        {
+            Thread.Sleep(2000);
+            balance = amount;
+        }
+
+        /*
+         * This funciton checks the account pin against the argument passed to it
+         *
+         * returns:
+         * true if they match
+         * false if they do not
+         */
+        public Boolean checkPin(int pinEntered)
+        {
+            if (pinEntered == pin)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        public int getAccountNum()
+        {
+            return accountNum;
+        }
+
+    }
+
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+
+
+        public static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+
+            Application.Run(new ATMMenu());
+        }
+    }
+
+
+}
